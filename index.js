36ec908402f1ed5fe0d572fdbdde91c08c2697ed
yasen <yasen50yasen50@gmail.com>
2024-05-24 09:08:58 +0300

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const PORT = 3001 || 5000;
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 
 app.listen(PORT,()=>{
     console.log('app listen on port ',PORT)
